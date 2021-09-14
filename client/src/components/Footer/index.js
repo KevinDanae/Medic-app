f28e0c59087dd9a2b175ba7b78b8416c335387cb
Kevin Aguilera <kevin.lovo@gmail.com>
2021-09-14 16:09:54 -0500

    Se cambiaron las propiedades clases por className

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer class="p-10 footer bg-base-200 text-base-content">
-        <div class=""> 
-          <i class="fas fa-user-md text-7xl ml-10" ></i>
+      <footer className="p-10 footer bg-base-200 text-base-content">
+        <div className="">
+          <i className="fas fa-user-md text-7xl ml-10"></i>
           <p>
             Medic App S.A de C.V.
             <br />
@@ -13,20 +13,20 @@ const Footer = () => {
           </p>
         </div>
         <div>
-          <span class="footer-title">Servicios</span>
-          <a class="link link-hover">Asitencia</a>
-          <a class="link link-hover">Citas</a>
+          <span className="footer-title">Servicios</span>
+          <a className="link link-hover">Asitencia</a>
+          <a className="link link-hover">Citas</a>
         </div>
         <div>
-          <span class="footer-title">Compañia</span>
-          <a class="link link-hover">Sobre Nosotros</a>
-          <a class="link link-hover">Contacto</a>
+          <span className="footer-title">Compañia</span>
+          <a className="link link-hover">Sobre Nosotros</a>
+          <a className="link link-hover">Contacto</a>
         </div>
         <div>
-          <span class="footer-title">Legal</span>
-          <a class="link link-hover">Terminos y Condiciones</a>
-          <a class="link link-hover">Politicas de privacidad</a>
-          <a class="link link-hover">Politicas de Cookies</a>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terminos y Condiciones</a>
+          <a className="link link-hover">Politicas de privacidad</a>
+          <a className="link link-hover">Politicas de Cookies</a>
         </div>
       </footer>
     </>
