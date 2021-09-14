568ad1ae478915c13f0268d036e07245c52fa96a
Gabriel-F-D <gdelgadillo299@gmail.com>
2021-09-14 17:45:00 -0300

    fix lp

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-
+import LandingPage from "./components/LandingPage";
 import { Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -9,6 +9,7 @@ function App() {
       {/* Aqui va el navbar global para que se vea en todas las rutas */}
       <Route exact path="/">
         {/* Aqui va la pagina princpipal de landing page */}
+        <LandingPage />
       </Route>
       <Route exact path="/home">
         {/*  Aqui va el home dependiendo del tipo de usario */}
