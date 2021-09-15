615fe5bd84f5819280ace3af3f3c097b43de5e75
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-09-15 13:15:19 -0500

    tests removed

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,30 +6,8 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.get("/", (req, res) =>{
-  res.json("soy la ruta get")
-})
 
-router.post("/patient", async(req, res) => {
-  const {identification, name, lastname, age, plan, status, email, mobil} = req.body 
-  try {
-    if( identification && name && lastname && email &&age && plan && status){
-      const newPatient = await Patient.create({
-        identification,
-        name,
-        lastname,
-        email,
-        mobil,
-        age,
-        plan,
-        status
-      })
-      res.json(newPatient)
-    }
-  } catch (error) {
-    console.log(error)
-    res.status(404).json(error)
-  }
-})
+//router.use('/blood', bloodRouter)
+//router.use('/doctor',doctorRouter);
 
 module.exports = router;
