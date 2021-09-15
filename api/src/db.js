ddfe99f19828f5d2ffcaf71eae519e9aaf195cef
jcantini <jorge.cantinigmail.com>
2021-09-15 10:06:06 -0300

    Camios Modelo Doctor

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,8 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/medic_app`, {
+//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/medic_app`, {
+  const sequelize = new Sequelize('postgres://postgres:stk0386@localhost:5432/medic_app', {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
