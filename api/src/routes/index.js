aed2a631be476bbe6efc3bf506b9991f6490ffe8
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-09-15 08:06:01 -0500

    Pruebas del modelo

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { Patient } = require('../db');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
@@ -7,6 +8,26 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 router.get("/", (req, res) =>{
   res.json("soy la ruta get")
+})
+
+router.post("/patient", async(req, res) => {
+  const {identification, name, lastname, age, plan, status} = req.body 
+  try {
+    if( identification && name && lastname && age && plan && status){
+      const newPatient = await Patient.create({
+        identification,
+        name,
+        lastname,
+        age,
+        plan,
+        status
+      })
+      res.json(newPatient)
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(404).json(error)
+  }
 })
 
 module.exports = router;
