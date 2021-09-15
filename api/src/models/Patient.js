2288001643727f350e1c832e65b5d098def710d7
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-09-14 20:53:47 -0500

    initial patient model

--- api/src/models/Patient.js
+++ api/src/models/Patient.js
@@ -4,6 +4,24 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) =>{
   sequelize.define('patient', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    identification:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate:{
+        isNum:{
+          msg: "la identificación debe ser un número"
+        },
+        isInt: {
+          msg: "Debería ser un entero"
+        }
+      }
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -16,5 +34,62 @@ module.exports = (sequelize) =>{
         }
       }
     },
+    lastname:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "El apellido no debe estar vacio"
+        },
+        isAlpha: {
+          msg: "El apellido debe ser exclusivamente caracteres alfabéticos",
+        }
+      }
+    },
+    fullname:{
+      type: DataTypes.VIRTUAL,
+      get: function(){
+        return `${this.name} ${this.lastname}`
+      }
+    },
+    age:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate:{
+        notNull: {
+          msg: "La edad no debe estar vacía"
+        },
+        isInt: {
+          msg: "Debería ser un entero"
+        }
+      }
+    },
+    state:{
+      type: DataTypes.ENUM("alive", "dead")
+    },
+    height: {
+      type: DataTypes.FLOAT,
+      validate:{
+        isFloat: {
+          msg: "Debería ser un flotante"
+        }
+      }
+    },
+    weight: {
+      type: DataTypes.FLOAT,
+      validate:{
+        isFloat: {
+          msg: "Debería ser un flotante"
+        }
+      }
+    },
+    plan:{
+      type: DataTypes.ENUM("subsidized", "contributory"),
+      allowNull: false,
+    },
+    status:{
+      type: DataTypes.BOOLEAN, 
+    }
+
   });
 }
